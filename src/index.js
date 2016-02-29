9aa9d0aa5348f0b363b41c9886f4bd577eaeb475
Filatov Evgen <evgen.filatov@gmail.com>
2016-03-01 00:01:59 +0200

    listening to global keys

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
+const {ipcRenderer} = electron;
 
+ipcRenderer.on('key', (e, m) => console.log(m));
 class App extends React.Component {
 	render() {
 		return <h1>React!</h1>;
