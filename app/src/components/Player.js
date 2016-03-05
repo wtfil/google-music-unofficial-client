6f5a1b9e3dd8c4561d962ef98f2a01e82118435d
Filatov Evgen <evgen.filatov@gmail.com>
2016-03-05 12:20:00 +0200

    added progress

--- app/src/components/Player.js
+++ app/src/components/Player.js
@@ -6,6 +6,11 @@ import {pausePlay} from '../actions';
 class Audio extends React.Component {
 	componentWillMount(props) {
 		this.audio = new window.Audio();
+		this.audio.addEventListener('timeupdate', e => {
+			this.props.onProgress({
+				progress: this.audio.currentTime /this.audio.duration
+			});
+		});
 		this.onProps(this.props);
 	}
 	componentWillUnmount() {
@@ -20,6 +25,10 @@ class Audio extends React.Component {
 			if (props.streamUrl && this.audio.src !== props.streamUrl) {
 				this.audio.src = props.streamUrl;
 			}
+			const position = props.progress * this.audio.duration;
+			if (Math.abs(position - this.audio.currentTime) > 1) {
+				this.audio.currentTime = position;
+			}
 			this.audio.play();
 		} else {
 			this.audio.pause();
@@ -32,14 +41,38 @@ class Audio extends React.Component {
 
 @connect(state => state)
 export default class Player extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			progress: 0
+		};
+	}
+
+	onProgress(e) {
+		this.setState(e);
+	}
+
+	setProgress(e) {
+		const progress = e.pageX / e.target.offsetWidth;
+		this.setState({progress});
+	}
+
 	render() {
 		const {music, player, dispatch} = this.props;
+		const {progress} = this.state;
 		let currentSong;
 		if (player.trackId) {
 			currentSong = music.songs.find(item => item.trackId === player.trackId);
 		}
 		return <footer className="player">
-			<Audio {...player} />
+			<Audio
+				{...player}
+				progress={progress}
+				onProgress={::this.onProgress}
+			/>
+			<div onClick={::this.setProgress} className="player__progress pointer progress grey lighten-3">
+				<div className="determinate orange" style={{width: 100 * progress + '%'}}></div>
+			</div>
 			<div className="player__content collection-item valign-wrapper">
 				<div className="player__left">
 					{currentSong &&
@@ -68,4 +101,5 @@ export default class Player extends React.Component {
 			</div>
 		</footer>;
 	}
+
 }
