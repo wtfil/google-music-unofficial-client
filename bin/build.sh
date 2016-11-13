fdfd7661cdb71bf98ea692bf33c7e6acf060c7b8
Evgen Filatov <evgen.filatov@gmail.com>
2016-11-13 18:15:10 +0200

    optimized build; archving windows build

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-IGNORE="node_modules/(electron-packager|electron-prebuilt|webpack.*|babel.*|react.*|redux.*|.*loader|less)"
+IGNORE="node_modules/((?!(playmusic|crypto-js|node-uuid)).)"
 IS_MAC=`env | grep Apple`
 
 if [[ $IS_MAC ]]; then
     PLATFORM=darwin
     ICON='./app/icon'
 else
+    rm -rf GoogleMusic*
     PLATFORM=win32
     ICON='app\icon.ico'
 fi
@@ -17,4 +18,9 @@ electron-packager . GoogleMusic \
 if [[ $IS_MAC ]]; then
     cd GoogleMusic-darwin-x64
     tar -zcvf GoogleMusic.app.tar.gz GoogleMusic.app
+else
+    mkdir GoogleMusic
+    cp -r GoogleMusic-win32-x64/{content_shell.pak,GoogleMusic.exe,icudtl.dat,locales,natives_blob.bin,node.dll,resources,ui_resources_200_percent.pak,version} GoogleMusic
+    powershell "Add-Type -A System.IO.Compression.FileSystem;    [IO.Compression.ZipFile]::CreateFromDirectory('GoogleMusic', 'GoogleMusic.zip')"
+    rm -rf GoogleMusic GoogleMusic-win32-x64
 fi
